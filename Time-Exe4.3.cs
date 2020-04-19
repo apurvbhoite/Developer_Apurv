f9199bd8d9819e28c7de5db2316d1b58f8017d35
Apurv Bhoite <62795575+apurvbhoite@users.noreply.github.com>
2020-04-19 18:36:03 +0530

    4 Codes Added

---
+++ Time-Exe4.3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter time: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            var components = input.Split(':');
+            if (components.Length != 2)
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            try
+            {
+                var hour = Convert.ToInt32(components[0]);
+                var minute = Convert.ToInt32(components[1]);
+
+                if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+                    Console.WriteLine("Ok");
+                else
+                    Console.WriteLine("Invalid Time");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Time");
+            }
+        }
+    }
+}
---
+++ Vowels-Exe4.5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            // Note the ToLower() here. This is used to count for both A and a. 
+            var input = Console.ReadLine().ToLower();
+
+            var vowels = new List<char>(new char[] { 'a', 'e', 'o', 'u', 'i' });
+            var vowelsCount = 0;
+            foreach (var character in input)
+            {
+                if (vowels.Contains(character))
+                    vowelsCount++;
+            }
+
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
